3dadad4d5fc7680d11dff63d3e93ca7fa21b4712
Jason <jmaalegado@gmail.com>
2021-08-10 10:20:28 +1000

    ng-book-proj01 - first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Article  } from './article/article.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  articles: Article[];
+
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+     this.articles.push(
+       new Article(title.value, link.value)
+     ) 
+
+    title.value = "";
+    link.value = "";
+    return false;
+  }
+
+  constructor() {
+    this.articles = [
+      new Article('Angular', 'http://angular.io', 5),
+      new Article('ReactJS', 'https://reactjs.org', 8),
+      new Article('VueJS', 'https://vuejs.org', 2),
+      new Article('GraphQL', 'https://graphql.org', 3),
+      new Article('PHP', 'http://php.net', 8)
+    ]
+  }  
+}
